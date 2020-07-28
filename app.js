347538af937db29c991e3b6887e645b27f044134
luoqiufen <luoq6868@163.com>
2020-07-28 20:30:36 +0800

    aaa

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-07-22 15:50:12
- * @LastEditTime: 2020-07-23 20:05:49
+ * @LastEditTime: 2020-07-28 19:14:47
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \photo\app.js
@@ -22,3 +22,8 @@ app.use(express.urlencoded({extended:true}));
 // 设置根目录
 app.use(express.static('./uploads'));
 app.use(express.static('./public'));
+
+// 处理 / 请求,展示相册首页
+app.get('/',function(req,res){
+    res.redirect('/dir')
+})
